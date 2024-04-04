9be13b1e4cf5979f25c7121527f6a8d247f090eb
Kirill Ivanov <bragov4ik@hotmail.com>
2024-04-05 01:10:14 +0300

    adaptive whitelist tagging

--- src/whatever.rs
+++ src/whatever.rs
@@ -3,7 +3,7 @@ use snafu::{Backtrace, Snafu};
 // Just like `snafu::Whatever` but `+ Send + Sync`
 #[derive(Debug, Snafu)]
 #[snafu(whatever)]
-#[snafu(display("{message:?}"))]
+#[snafu(display("{message}"))]
 #[snafu(provide(opt, ref, chain, dyn std::error::Error + Send + Sync => source.as_deref()))]
 pub struct Whatever {
     #[snafu(source(from(Box<dyn std::error::Error + Send + Sync>, Some)))]
