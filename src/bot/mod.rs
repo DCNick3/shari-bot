cce66fb51cc9de1214ef27d43f65affde9e300f6
Nikita Strygin <nikita6@bk.ru>
2024-05-16 20:26:07 +0300

    Actually move all parts of the uploader to upload.rs

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -7,23 +7,17 @@ pub mod whitelist;
 pub use self::upload::UploadNotifier;
 pub use self::upload::UploadStatus;
 use crate::bot::lang::Lang;
-use crate::downloader::{BytesStream, VideoDownloadResult};
-use crate::{
-    bot::commands::handle_command, dispatcher::DownloadDispatcher, downloader::Downloader,
-    whatever::Whatever,
-};
-use futures::TryStreamExt as _;
+use crate::{bot::commands::handle_command, dispatcher::DownloadDispatcher, whatever::Whatever};
 use grammers_client::{
     button, reply_markup,
-    types::{Attribute, Chat, Message},
+    types::{Chat, Message},
     Client, InputMessage, Update,
 };
 use grammers_tl_types::enums;
 use serde::{Deserialize, Serialize};
 use snafu::{ResultExt as _, Snafu};
 use std::{collections::HashSet, sync::Arc, time::Duration};
-use tokio::sync::{watch::Receiver, Mutex};
-use tokio_util::compat::FuturesAsyncReadCompatExt as _;
+use tokio::sync::Mutex;
 use tracing::{debug, error, info, instrument, warn};
 use url::Url;
 
@@ -74,119 +68,6 @@ pub async fn run_bot(
 
 fn make_keyboard(link_text: &str, url: Url) -> reply_markup::Inline {
     reply_markup::inline(vec![vec![button::url(link_text, url)]])
-}
-
-async fn upload_video(
-    bot: &Client,
-    downloader: Arc<dyn Downloader>,
-    url: Url,
-    initial_message: &Message,
-    notifier: UploadNotifier,
-) -> Result<(), Whatever> {
-    let link_text = downloader.link_text();
-
-    let VideoDownloadResult {
-        canonical_url,
-        video_information,
-        video_stream: BytesStream { stream, size },
-    } = downloader.download(url.clone(), notifier).await?;
-    let mut stream = stream.into_async_read().compat();
-
-    debug!("Uploading the stream to telegram...");
-    let uploaded_video = bot
-        .upload_stream(&mut stream, size as usize, "video.mp4".to_string())
-        .await
-        .whatever_context("Uploading video")?;
-
-    debug!("Sending the video message...");
-    let mut message = InputMessage::text("")
-        .document(uploaded_video)
-        .reply_markup(&make_keyboard(link_text, canonical_url));
-    if let Some(video_information) = video_information {
-        // big files require this information
-        // short videos can be sent without it
-        // we only return this from youtube videos for now
-        message = message.attribute(Attribute::Video {
-            h: video_information.height,
-            w: video_information.width,
-            duration: video_information.duration,
-            round_message: false,
-            supports_streaming: true,
-        });
-    }
-
-    initial_message
-        .reply(message)
-        .await
-        .whatever_context("Sending video message")?;
-
-    debug!("Successfully sent video!");
-
-    Ok(())
-}
-
-struct StatusMessageState {
-    magic_index: usize,
-    status_receiver: Receiver<UploadStatus>,
-    previous_text: Option<String>,
-}
-
-impl StatusMessageState {
-    const MAGIC_PARTS: &'static [&'static str] = &[":｡", "･:*", ":･ﾟ", "’★,｡", "･:*", ":･ﾟ", "’☆"];
-
-    pub fn new(status_receiver: Receiver<UploadStatus>) -> Self {
-        Self {
-            magic_index: 1,
-            status_receiver,
-            previous_text: None,
-        }
-    }
-
-    fn format_progress_bar(progress: f32) -> String {
-        const PROGRESS_BAR_LENGTH: u32 = 30;
-
-        let progress = (progress * PROGRESS_BAR_LENGTH as f32).round() as u32;
-
-        let filled = (0..progress).map(|_| 'O').collect::<String>();
-        let empty = (progress..PROGRESS_BAR_LENGTH)
-            .map(|_| '.')
-            .collect::<String>();
-
-        let progressbar = format!("{}{}", filled, empty);
-
-        progressbar
-    }
-
-    pub fn update(&mut self) -> Option<InputMessage> {
-        self.magic_index += 1;
-        if self.magic_index == Self::MAGIC_PARTS.len() {
-            self.magic_index = 0;
-        }
-
-        let magic = &Self::MAGIC_PARTS[..self.magic_index];
-        let magic = magic.join("");
-        let message = format!("{} {}", Lang::StatusWorking, magic);
-
-        let status = self.status_receiver.borrow_and_update();
-
-        let body = match *status {
-            UploadStatus::FetchingLink => Lang::StatusGettingLink.to_string(),
-            UploadStatus::Uploading { progress } => {
-                markdown::code_inline(&Self::format_progress_bar(progress))
-            }
-        };
-
-        let message = format!("{}\n\n{}", message, body);
-        if let Some(previous_text) = &self.previous_text {
-            if previous_text == &message {
-                return None;
-            }
-        }
-        let result = InputMessage::markdown(&message);
-        self.previous_text = Some(message);
-
-        Some(result)
-    }
 }
 
 #[derive(Debug, Snafu)]
--- src/bot/upload.rs
+++ src/bot/upload.rs
@@ -1,16 +1,18 @@
 use crate::bot;
-use crate::bot::{StatusMessageState, UploadError};
-use crate::downloader::Downloader;
+use crate::bot::lang::Lang;
+use crate::bot::{markdown, UploadError};
+use crate::downloader::{BytesStream, Downloader, VideoDownloadResult};
 use crate::whatever::Whatever;
-use futures::FutureExt;
-use grammers_client::types::Message;
-use grammers_client::Client;
+use futures::{FutureExt, TryStreamExt};
+use grammers_client::types::{Attribute, Message};
+use grammers_client::{Client, InputMessage};
 use snafu::{FromString, ResultExt};
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::select;
 use tokio::sync::watch::{Receiver, Sender};
 use tokio::time::timeout;
+use tokio_util::compat::FuturesAsyncReadCompatExt;
 use tracing::{debug, info_span, instrument, Instrument};
 use url::Url;
 
@@ -49,7 +51,7 @@ pub async fn upload_with_status_updates(
 ) -> Result<(), UploadError> {
     let (notifier, notification_rx) = UploadNotifier::make();
 
-    let upload_fut = bot::upload_video(client, downloader, url, initial_message, notifier)
+    let upload_fut = upload_video(client, downloader, url, initial_message, notifier)
         .fuse()
         .instrument(info_span!("upload_video"));
     let upload_fut = timeout(video_handling_timeout, upload_fut);
@@ -86,5 +88,118 @@ pub async fn upload_with_status_updates(
                 Err(_) => return Err(UploadError::Timeout),
             }
         }
+    }
+}
+
+async fn upload_video(
+    bot: &Client,
+    downloader: Arc<dyn Downloader>,
+    url: Url,
+    initial_message: &Message,
+    notifier: UploadNotifier,
+) -> Result<(), Whatever> {
+    let link_text = downloader.link_text();
+
+    let VideoDownloadResult {
+        canonical_url,
+        video_information,
+        video_stream: BytesStream { stream, size },
+    } = downloader.download(url.clone(), notifier).await?;
+    let mut stream = stream.into_async_read().compat();
+
+    debug!("Uploading the stream to telegram...");
+    let uploaded_video = bot
+        .upload_stream(&mut stream, size as usize, "video.mp4".to_string())
+        .await
+        .whatever_context("Uploading video")?;
+
+    debug!("Sending the video message...");
+    let mut message = InputMessage::text("")
+        .document(uploaded_video)
+        .reply_markup(&bot::make_keyboard(link_text, canonical_url));
+    if let Some(video_information) = video_information {
+        // big files require this information
+        // short videos can be sent without it
+        // we only return this from youtube videos for now
+        message = message.attribute(Attribute::Video {
+            h: video_information.height,
+            w: video_information.width,
+            duration: video_information.duration,
+            round_message: false,
+            supports_streaming: true,
+        });
+    }
+
+    initial_message
+        .reply(message)
+        .await
+        .whatever_context("Sending video message")?;
+
+    debug!("Successfully sent video!");
+
+    Ok(())
+}
+
+struct StatusMessageState {
+    magic_index: usize,
+    status_receiver: Receiver<UploadStatus>,
+    previous_text: Option<String>,
+}
+
+impl StatusMessageState {
+    const MAGIC_PARTS: &'static [&'static str] = &[":｡", "･:*", ":･ﾟ", "’★,｡", "･:*", ":･ﾟ", "’☆"];
+
+    pub fn new(status_receiver: Receiver<UploadStatus>) -> Self {
+        Self {
+            magic_index: 1,
+            status_receiver,
+            previous_text: None,
+        }
+    }
+
+    fn format_progress_bar(progress: f32) -> String {
+        const PROGRESS_BAR_LENGTH: u32 = 30;
+
+        let progress = (progress * PROGRESS_BAR_LENGTH as f32).round() as u32;
+
+        let filled = (0..progress).map(|_| 'O').collect::<String>();
+        let empty = (progress..PROGRESS_BAR_LENGTH)
+            .map(|_| '.')
+            .collect::<String>();
+
+        let progressbar = format!("{}{}", filled, empty);
+
+        progressbar
+    }
+
+    pub fn update(&mut self) -> Option<InputMessage> {
+        self.magic_index += 1;
+        if self.magic_index == Self::MAGIC_PARTS.len() {
+            self.magic_index = 0;
+        }
+
+        let magic = &Self::MAGIC_PARTS[..self.magic_index];
+        let magic = magic.join("");
+        let message = format!("{} {}", Lang::StatusWorking, magic);
+
+        let status = self.status_receiver.borrow_and_update();
+
+        let body = match *status {
+            UploadStatus::FetchingLink => Lang::StatusGettingLink.to_string(),
+            UploadStatus::Uploading { progress } => {
+                markdown::code_inline(&Self::format_progress_bar(progress))
+            }
+        };
+
+        let message = format!("{}\n\n{}", message, body);
+        if let Some(previous_text) = &self.previous_text {
+            if previous_text == &message {
+                return None;
+            }
+        }
+        let result = InputMessage::markdown(&message);
+        self.previous_text = Some(message);
+
+        Some(result)
     }
 }
